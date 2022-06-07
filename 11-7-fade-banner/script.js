632a8334112b6811f21f27806bedc10039016a26
kuas1091104337 <kuas1091104337@gmail.com>
2022-06-08 01:50:16 +0800

    6/8 setup

--- 11-7-fade-banner/script.js
+++ 11-7-fade-banner/script.js
@@ -4,26 +4,29 @@
 // 	$(".small img").click(function(){
 // 		let bnNum = $(this).index() - 1;
 // 		$(".banner img").not("#PP_"+bnNum).fadeOut().eq(bnNum).fadeIn();
-// 	});		
+// 	});
 // });
 
-window.onload=function(){
-	function findIndex(el) {
-		if(!el) return -1;
-		let i = 1;
-		while(el = el.previousElementSibling) i++;
-		return i;
-	}
-	document.getElementById('PP_1').classList.add('show');
-	document.querySelectorAll('.small > img').forEach((el) => {
-		el.addEventListener('click',bnShow);
-	});
-	function bnShow(e){
-		console.log(findIndex(e.target));
-		// console.log(e.target);
-		document.querySelectorAll('.banner > img').forEach((el) => {
-			if(el.classList.contains('show')) el.classList.remove('show');
-		});
-		document.getElementById('PP_'+findIndex(e.target)).classList.add('show');
-	}
-}
+window.onload = function () {
+  function findIndex(e) {
+    let el = e.target, i = 1;
+    // if (!el) return -1;
+    // let i = 1;
+    while ((el = el.previousElementSibling)) i++;
+    return i;
+  }
+  document.getElementById('PP_1').classList.add('show');
+  document.querySelectorAll('.small > img').forEach((el) => {
+    el.addEventListener("click", bnShow);
+  });
+  function bnShow(e) {
+    // console.log(e.target);
+    // console.log(findIndex(e));
+    console.log(findIndex(e));
+    document.querySelectorAll(".banner > img").forEach((el) => {
+      if (el.classList.contains("show")) el.classList.remove("show");
+    });
+    // document.getElementById("PP_" + findIndex(e.target)).classList.add("show");
+    document.getElementById("PP_" + findIndex(e)).classList.add("show");
+  }
+};
--- 15-2-scrollTop-section/script.js
+++ 15-2-scrollTop-section/script.js
@@ -1,19 +1,19 @@
 // $(function(){
 // 	$("#B00").click(function(){
 // 		$("html,body").stop().animate({scrollTop:0},800);
-// 		return false;	
+// 		return false;
 // 	});
 // 	$("#B01").click(function(){
 // 		$("html,body").stop().animate({scrollTop:1000},800);
-// 		return false;	
+// 		return false;
 // 	});
 // 	$("#B02").click(function(){
 // 		$("html,body").stop().animate({scrollTop:2000},800);
-// 		return false;	
+// 		return false;
 // 	});
 // 	$("#B03").click(function(){
 // 		$("html,body").stop().animate({scrollTop:3000},800);
-// 		return false;	
+// 		return false;
 // 	});
 // });
 // $(function(){
@@ -23,65 +23,60 @@
 // 	// }
 // 	$("#MENU a").click(function(){
 // 		var Number = this.id.substr(-1);
-// 		$("html,body").stop().animate({scrollTop:Number*1000},600);		
+// 		$("html,body").stop().animate({scrollTop:Number*1000},600);
 // 	});
 // });
-window.onload = function(){
-	const scrollTime = 600, menuH = document.getElementById('MENU').offsetHeight;
-	let targetPos;
-	document.querySelectorAll('#MENU > li').forEach((el) => {
-		el.addEventListener('click',menuGo);
-	});
-	function menuGo(e){
-		// console.log(e.target.className); console.log(e.target.id);
-		// document.querySelector('main>div:nth-of-type(4)').offsetTop
-		switch(e.target.id) {
-			case 'B00' :
-				targetPos = document.getElementById('AAA').offsetTop - menuH;
-				// document.scrollingElement.scrollTop
-				break;
-			case 'B01' :
-				targetPos = document.getElementById('BBB').offsetTop - menuH;
-				break;
-			case 'B02' :
-				targetPos = document.getElementById('CCC').offsetTop - menuH;
-				break;
-			case 'B03' :
-				targetPos = document.getElementById('DDD').offsetTop - menuH;
-				break;
-		}
-		console.log(targetPos);
-		scrollTopAnimateLinear(targetPos,scrollTime)
-	}
-	function scrollTopAnimateLinear(target,duration){
-		// 如果已經在頂部，則取消
-		if (document.scrollingElement.scrollTop === target) return;
-		const totalScrollDistance = target - document.scrollingElement.scrollTop;//,
-					// moveDirection = totalScrollDistance < 0 ? 'UP' : 'DOWN';
-		// scrollY現在的位子
-		let scrollY = document.scrollingElement.scrollTop, oldTimestamp = null;
-		console.log('tar:'+target); console.log('dis:'+totalScrollDistance); //console.log(moveDirection);
-		// 滾動Y軸=總滾動距離, 舊時間戳 directStatus
-		function step(newTimestamp){ //步驟
-			if (oldTimestamp !== null){
-				// console.log('newTime : '+newTimestamp); console.log('oldTime : '+oldTimestamp);
-				//if duration is 0 scrollY will be -Infinity
-				let progress = (newTimestamp-oldTimestamp)/duration
-				scrollY = scrollY + totalScrollDistance * progress;
-				console.log('dis'+totalScrollDistance); console.log('sY'+scrollY);
-				// if(moveDirection === 'UP' && scrollY < target) return document.scrollingElement.scrollTop = target;
-				// if(moveDirection === 'DOWN' && scrollY > target) return document.scrollingElement.scrollTop = target;
-				// if(totalScrollDistance < 0 && scrollY < target) return document.scrollingElement.scrollTop = target;
-				// if(totalScrollDistance > 0 && scrollY > target) return document.scrollingElement.scrollTop = target;
-				if(totalScrollDistance<0 && scrollY<target || totalScrollDistance>0 && scrollY>target) return document.scrollingElement.scrollTop = target;
-				document.scrollingElement.scrollTop = scrollY;
-			}
-			oldTimestamp = newTimestamp;
-			window.requestAnimationFrame(step);
-		}
-		window.requestAnimationFrame(step);
-	}
-}
+window.onload = function () {
+  const scrollTime = 600,
+    menuH = document.getElementById("MENU").offsetHeight;
+  document.querySelectorAll("#MENU > li").forEach((el) => {
+    el.addEventListener("click", menuGo);
+  });
+  function findIndex(e) {
+    // if (!el) return -1;
+    // let i = 1;
+    let el = e.target, i = 1;
+    while ((el = el.previousElementSibling)) i++;
+    return i;
+  }
+  function menuGo(e) {
+    console.log(e.target);
+    // console.log(e.target.className); console.log(e.target.id);
+    let num = findIndex(e), target;
+    target = document.querySelector('body>div:nth-of-type(' + num + ')').offsetTop - menuH;
+    scrollTopAnimateLinear(target, scrollTime);
+  }
+  function scrollTopAnimateLinear(target, duration) {
+    // 如果已經在頂部，則取消
+    if (document.scrollingElement.scrollTop === target) return;
+    const totalScrollDistance = target - document.scrollingElement.scrollTop; //,
+    // windowScrollY現在的位子
+    let windowScrollY = document.scrollingElement.scrollTop,
+      oldTimestamp = null;
+    console.log("tar:" + target);
+    console.log("dis:" + totalScrollDistance); //console.log(moveDirection);
+    // 滾動Y軸=總滾動距離, 舊時間戳 directStatus
+    function step(newTimestamp) {
+      //步驟
+      if (oldTimestamp !== null) {
+        // console.log('newTime : '+newTimestamp); console.log('oldTime : '+oldTimestamp);
+        //if duration is 0 windowScrollY will be -Infinity
+        let progress = (newTimestamp - oldTimestamp) / duration;
+        // windowScrollY = windowScrollY + totalScrollDistance * progress;
+        windowScrollY += totalScrollDistance * progress;
+        // console.log("dis" + totalScrollDistance);
+        // console.log("sY" + windowScrollY);
+        // if(totalScrollDistance < 0 && windowScrollY < target) return document.scrollingElement.scrollTop = target;
+        // if(totalScrollDistance > 0 && windowScrollY > target) return document.scrollingElement.scrollTop = target;
+        if ((totalScrollDistance < 0 && windowScrollY < target) || (totalScrollDistance > 0 && windowScrollY > target)) return (document.scrollingElement.scrollTop = target);
+        document.scrollingElement.scrollTop = windowScrollY;
+      }
+      oldTimestamp = newTimestamp;
+      window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+  }
+};
 
 //test-1
 // window.onload = function(){
